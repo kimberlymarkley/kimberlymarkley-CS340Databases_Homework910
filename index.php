ebac5c04275ad42532546e62c691a2cc49fbb70a
Melissa Swearingen <swearingenmj@gmail.com>
2022-06-07 16:32:43 -0700

    added 'welcome', an explanation of the website, and the ability to display queried data

--- index.php
+++ index.php
@@ -1,57 +1,21 @@
 <?php include 'header.php'; ?>
 
 <main>
+    <div id="featured">
+        <h2>Welcome to the Celestial Body Database</h2>
+        <p>
+            This database contains information about The Solar System.
+            Each planet has a name, a radius, a mass, and a distance from the sun.
+        </p>
+    </div>
+
 
 <?php
    $db = new SQLite3('hw9.db');
    if($db->connect_error) {
       die("Error: Could not connect to database. " . $db->connect_error);
    }
-
-//    For each table, write a query that returns any 5 records.
-//    Use the command LIMIT 5 to control the result count.
-//    You should return all attributes and the default attribute names using SELECT * syntax.
-//    You may merely choose to return the first five records, or
-//        use the WHERE clause to specify a more specific and interesting query.
-//    Cite any and all real-world data sources (the URL) you use as a comment in your .sql file
-
-
-$sql_planets = "SELECT * FROM PLANET ORDER BY RANDOM() LIMIT 5";
-$planets = $db->query($sql_planets);
-
 ?>
-<table id="motorcycles">
-    <thead>
-        <tr>
-            <th>Planet ID</th>
-            <th>Name</th>
-            <th>Mass</th>
-            <th>Diameter</th>
-            <th>Gravity</th>
-            <th>Orbital Period</th>
-            <th>Day Length</th>
-            <th>Dwarf Planet</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            while($row = $planets->fetchArray(SQLITE3_ASSOC)) {
-                echo "<tr>";
-                foreach($row as $field) {
-                    echo "<td>$field</td>";
-                }
-                echo "</tr>";
-            }
-        ?>
-        <tr>
-            <td colspan="8">
-                <a href="planets.php">View All Planets</a>
-            </td>
-        </tr>
-    </tbody>
-</table>
-
-
 
 <form action="?" method="post">
     <input style="width: XX%" type="text" name="query" placeholder="SELECT * FROM TableName">
@@ -59,16 +23,27 @@ $planets = $db->query($sql_planets);
 </form>
 
 <pre>
-<?php 
-    if (isset($_POST['query'])) {
-        $query = $_POST['query'];
-        $res = $db->query($query);
-        while($row = $res->fetchArray(SQLITE3_ASSOC)) {
-            print_r($row);
-            echo "<br>";
-        }
-    }
-?>
+    <table id="motorcycles">
+        <thead>
+            <tr>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                if (isset($_POST['query'])) {
+                    $query = $_POST['query'];
+                    $res = $db->query($query);
+                    while($row = $res->fetchArray(SQLITE3_ASSOC)) {
+                        echo "<tr>";
+                        foreach($row as $field) {
+                            echo "<td>$field</td>";
+                        }
+                        echo "</tr>";
+                    }
+                }
+            ?>
+        </tbody>
+    </table>
 </pre>
 </main>
 
