1539408623e889e88d4f22197e8047add36609e9
Kimboslice <kimboslice@Kimboslices-MacBook-Pro.local>
2022-06-06 22:15:02 -0700

    add values to PLANET and ARTIFICIAL_SATELLITE

--- hw9.sql
+++ hw9.sql
@@ -21,6 +21,10 @@ Homework 9 & 10
  *    used the command "sqlite3 hw9.db < hw9.sql" to run and check for syntax errors
  */
 
+/* all size data is relative to Earth, and all time 
+measurement is relative to Earth's day length
+*/
+
 DROP TABLE if exists ARTIFICIAL_SATELLITE;
 CREATE TABLE ARTIFICIAL_SATELLITE ( 
     Satid INTEGER NOT NULL,
@@ -35,7 +39,7 @@ CREATE TABLE SATELLITE_TASKS (
     Satid INTEGER NOT NULL,
     Task VARCHAR(50) NOT NULL,
     PRIMARY KEY (Satid, Task),
-    FOREIGN KEY (Satid) REFERENCES ARTIFICIAL_SATELLITE(Satid)
+    FOREIGN KEY (Satid) REFERENCES ARTIFICIAL_SATELLITE(Satid) ON DELETE CASCADE
 );
 
 DROP TABLE if exists ALIEN_LIFE;
@@ -51,9 +55,9 @@ DROP TABLE if exists STAR;
 CREATE TABLE STAR (
     Sid INTEGER NOT NULL,
     Star_name VARCHAR(50) NOT NULL,
-    Nass DOUBLE PRECISION NOT NULL,
-    Luminosity DOUBLE PRECISION NOT NULL,
-    Radius DOUBLE PRECISION NOT NULL,
+    Mass DOUBLE PRECISION(10,6) NOT NULL,
+    Luminosity DOUBLE PRECISION(10,6) NOT NULL,
+    Radius DOUBLE PRECISION(10,6) NOT NULL,
     PRIMARY KEY (Sid)
 );
 
@@ -61,12 +65,12 @@ DROP TABLE if exists PLANET;
 CREATE TABLE PLANET (
     Pid INTEGER NOT NULL,
     Planet_name VARCHAR(50) NOT NULL,
-    Mass DOUBLE PRECISION NOT NULL,
-    Diameter DOUBLE PRECISION NOT NULL,
-    Gravity DOUBLE PRECISION NOT NULL,
-    Orbital_period DOUBLE PRECISION NOT NULL,
-    Day_length DOUBLE PRECISION NOT NULL,
-    Dwarf_planet BOOLEAN NOT NULL,
+    Mass DOUBLE PRECISION(10,6) NOT NULL,
+    Diameter DOUBLE PRECISION(10,6) NOT NULL,
+    Gravity DOUBLE PRECISION(10,6) NOT NULL,
+    Orbital_period DOUBLE PRECISION(10,6) NOT NULL,
+    Day_length DOUBLE PRECISION(10,6) NOT NULL,
+    Dwarf_planet TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (Pid)
 );
 
@@ -75,10 +79,10 @@ CREATE TABLE MOON (
     Mid INTEGER NOT NULL,
     Moon_name VARCHAR(50) NOT NULL,
     Pid INTEGER NOT NULL,
-    Mass DOUBLE PRECISION NOT NULL,
-    Radius DOUBLE PRECISION NOT NULL,
-    Gravity DOUBLE PRECISION NOT NULL,
-    Albedo DOUBLE PRECISION NOT NULL,
+    Mass DOUBLE PRECISION(10,6) NOT NULL,
+    Radius DOUBLE PRECISION(10,6) NOT NULL,
+    Gravity DOUBLE PRECISION(10,6) NOT NULL,
+    Albedo DOUBLE PRECISION(10,6) NOT NULL,
     PRIMARY KEY (Mid),
     FOREIGN KEY (Pid) REFERENCES PLANET(Pid)
 );
@@ -96,9 +100,9 @@ DROP TABLE if exists ASTEROID;
 CREATE TABLE ASTEROID (
     Aid INTEGER NOT NULL,
     Name VARCHAR(50) NOT NULL,
-    Mass INTEGER, 
-    Diameter INTEGER, 
-    Rotation_period INTEGER,
+    Mass DOUBLE PRECISION(10,6) NOT NULL, 
+    Diameter DOUBLE PRECISION(10,6) NOT NULL, 
+    Rotation_period DOUBLE PRECISION(10,6) NOT NULL,
     PRIMARY KEY (Aid)
 );
 
@@ -106,7 +110,7 @@ DROP TABLE if exists COMET;
 CREATE TABLE COMET (
     Cid INTEGER NOT NULL,
     Name VARCHAR(50) NOT NULL,
-    Mass INTEGER,
+    Mass DOUBLE PRECISION(10,6) NOT NULL,
     PRIMARY KEY (Cid)
 );
 
@@ -150,3 +154,29 @@ CREATE TABLE COLLIDES (
     FOREIGN KEY (Mid) REFERENCES MOON(Mid),
     FOREIGN KEY (Aid) REFERENCES ASTEROID(Aid)
 );
+
+/* Planet data from: 
+https://www.windows2universe.org/?page=/our_solar_system/planets_table.html
+and
+https://en.wikipedia.org/wiki/List_of_gravitationally_rounded_objects_of_the_Solar_System#endnote_Fnone
+*/
+INSERT INTO PLANET VALUES
+(1,'Mercury',0.055,0.382,0.38,87.969,58.646225,0),
+(2,'Venus',0.815,0.949,0.9,224.701,243.0187,0),
+(3,'Earth',1,1,1,365.256363,0.99726968,0),
+(4,'Mars',0.107,0.532,0.38,686.971,1.02595675,0),
+(5,'Jupiter',318,11.209,2.64,4332.59,0.41354,0),
+(6,'Saturn',95,9.44,0.93,10759.22,0.44401,0),
+(7,'Uranus',14.5,4.007,0.89,30688.5,0.71833,0),
+(8,'Neptune',17,3.883,1.12,60182,0.67125,0),
+(9,'Pluto',0.0022,0.180,0.06,9090483.5,6.3872,1);
+
+/* Satellite data from links here:
+https://en.wikipedia.org/wiki/Category:Lists_of_satellites
+*/
+INSERT INTO ARTIFICIAL_SATELLITE VALUES
+(1,'Aqua','USA','Carries six instruments to observe interactions among the four spheres for Earth systems: oceans, land, atmosphere, and biosphere.'),
+(2,'CALIPSO','France','Studies thickness of clouds and aerosols for understanding of how much air pollution is present and changes in compositions in the atmosphere.'),
+(3,'Chollian-2A','South Korea','Also known as Communication, Ocean and Meteorological Satellites (COMS). Used for communication, oceanography, and meteorological observation.'),
+(4, 'COSMO-SkyMed 4','Italy','Used for defense and security assurance in Italy and other countries, seismic hazard analysis, environmental disaster monitoring, and agricultural mapping.'),
+(5, 'NigeriaSat-1','Nigeria','Part of the worldwide Disaster Monitoring Constellation System.');
