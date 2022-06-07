75ddc573128e0dad02a1bd6d0bf383e4c3da94bc
Melissa Swearingen <swearingenmj@gmail.com>
2022-06-06 18:21:02 -0700

    added relational schema index page

---
+++ relational-schema/index.php
@@ -0,0 +1,8 @@
+<?php include '../header.php'; ?>
+
+<h2>Relational Schema</h2>
+<ul id="manufacturers">
+<a href="https://imgur.com/BNVcXWf"><img src="https://i.imgur.com/BNVcXWf.png" title="source: imgur.com" /></a>
+</ul>
+
+<?php include '../footer.php'; ?>
