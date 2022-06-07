fdadbb16f2c192bffd8e0ac41d634a8ece017ad8
Kimboslice <kimboslice@Kimboslices-MacBook-Pro.local>
2022-06-06 18:33:50 -0700

    erase notes

--- hw9.sql
+++ hw9.sql
@@ -18,14 +18,14 @@ Homework 9 & 10
  *    -CHECK clauses (if needed)
  *    -short comment linking the table creation command to the entity name of your ER diagram
  *    -include a DROP TABLE command before each CREATE TABLE command, to facilitate quick database re-generation
- *
+ *    used the command "sqlite3 hw9.db < hw9.sql" to run and check for syntax errors
  */
 
 CREATE TABLE ARTIFICIAL_SATELLITE ( 
     Satid INTEGER NOT NULL,
-    Sat_name VARCHAR(50) NOT NULL, /* Do all Artificial Satellites have names? */
-    Country_origin VARCHAR(50) NOT NULL, /* I think the countries or companies that own them would assign a number or name */
-    Task VARCHAR(50) NOT NULL, /*so NOT NULL makes sense to me, and VARCHAR can hold both letters and numbers - km */
+    Sat_name VARCHAR(50) NOT NULL,
+    Country_origin VARCHAR(50) NOT NULL,
+    Task VARCHAR(50) NOT NULL,
     PRIMARY KEY (Satid)
 );
 
@@ -77,9 +77,6 @@ CREATE TABLE MOON (
     FOREIGN KEY (Pid) REFERENCES PLANET(Pid)
 );
 
-/* used the command "sqlite3 hw9.db < hw9.sql" to run and check for syntax errors until 
-I started to get errors because not all the tables are created yet */
-
 CREATE TABLE MOON_ORBITS (
     Pid INTEGER NOT NULL,
     Mid INTEGER NOT NULL,
@@ -91,8 +88,8 @@ CREATE TABLE MOON_ORBITS (
 CREATE TABLE ASTEROID (
     Aid INTEGER NOT NULL,
     Name VARCHAR(50) NOT NULL,
-    Mass INTEGER, /* should we add NOT NULL to Mass, Diameter, Rotation? */
-    Diameter INTEGER, /* Not sure we'll have all that data for each one -km */
+    Mass INTEGER, 
+    Diameter INTEGER, 
     Rotation_period INTEGER,
     PRIMARY KEY (Aid)
 );
