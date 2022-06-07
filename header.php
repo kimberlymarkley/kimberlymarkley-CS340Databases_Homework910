8f13f560e26ba64bf16e00aa3e3b3972bd02f382
Kimboslice <kimboslice@Kimboslices-MacBook-Pro.local>
2022-06-06 18:00:38 -0700

    move css file into assets and update link in header.php

--- header.php
+++ header.php
@@ -2,10 +2,9 @@
 <html lang="en-us">
 <head>
   <title>Homework 9</title>
-  <link rel="stylesheet" type="text/css" media="all" href="/assets/stylesheets/main.css">
+  <link rel="stylesheet" type="text/css" media="all" href="/assets/main.css">
   <style>
   </style>
-  <!-- <script src="/assets/javascript/main.js"></script> -->
 </head>
 <body>
   <header>
