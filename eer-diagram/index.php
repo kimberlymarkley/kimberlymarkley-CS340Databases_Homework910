8579c26fc2d701b39f99fb8fa060b83d59c70c45
Melissa Swearingen <swearingenmj@gmail.com>
2022-06-06 18:20:45 -0700

    added index page for eer diagram

---
+++ eer-diagram/index.php
@@ -0,0 +1,8 @@
+<?php include '../header.php'; ?>
+
+<h2>EER Diagram</h2>
+<ul id="manufacturers">
+<a href="https://imgur.com/Z2cDHRe"><img src="https://i.imgur.com/Z2cDHRe.png" title="source: imgur.com" /></a>
+</ul>
+
+<?php include '../footer.php'; ?>
