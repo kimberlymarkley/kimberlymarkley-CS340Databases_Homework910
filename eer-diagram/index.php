671574d197e8d2245422abaeabe93b909743f046
Melissa Swearingen <swearingenmj@gmail.com>
2022-06-07 19:22:43 -0700

    updated ids and classes

--- eer-diagram/index.php
+++ eer-diagram/index.php
@@ -1,8 +1,8 @@
 <?php include '../header.php'; ?>
 
-<div id="featured">
+<div id="explanation">
     <h2>EER Diagram</h2>
-    <div id="motorcycle">
+    <div id="diagram">
         <a href="/assets/images/CS340-Homework9-EER-Diagram.png"><img src="/assets/images/CS340-Homework9-EER-Diagram.png" title="EER Diagram"></a>
     </div>
 </div>
